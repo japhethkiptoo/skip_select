90a92b18deab974c2699d33cf003d28bd8212fd5
Japheth Kiptoo <thygon6422@gmail.com>
2025-06-07 23:20:15 +0300

    Update: useFetch hook - dry for data fetch

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,6 +1,36 @@
+import { useEffect, useState } from "react";
+
 //for clean data fetching
+type Response = {
+  data: any;
+  loading: boolean;
+  error: string | null;
+};
 
 export const useFetch = (
   fetchFn: () => Promise<any>,
   deps: React.DependencyList[] = [],
-) => {};
+): Response => {
+  const [data, setData] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    //instantiation and calling of fetch func.
+    (async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        const response = await fetchFn();
+        setData(response);
+        setLoading(false);
+      } catch (e) {
+        setError((e as Error).message);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, deps);
+
+  return { data, loading, error };
+};
