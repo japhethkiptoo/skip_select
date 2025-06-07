fdc83ef4db7fe64b811e72d8010ae892168ccd89
Japheth Kiptoo <thygon6422@gmail.com>
2025-06-07 22:26:31 +0300

    Update: skipOptions - display all skip options

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+import { SkipOptions } from "./components/skipOptions";
+
+const App = () => {
+  return (
+    <div className="min-h-screen  bg-[#121212] text-white">
+      <main className="py-8">
+        <SkipOptions />
+      </main>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/skipOptions.tsx
@@ -0,0 +1,14 @@
+export const SkipOptions = () => {
+  return (
+    <div className="max-w-5xl mx-auto flex flex-col gap-6">
+      <div className=" flex flex-col items-center">
+        <h2 className="text-3xl font-bold mb-4">Choose your skip size</h2>
+        <p className="text-xl text-gray-400">
+          Select the skip size that best suits your needs.
+        </p>
+      </div>
+
+      <div>list skips here</div>
+    </div>
+  );
+};
