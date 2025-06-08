0c111bb220871cd0236a6bf688962f05c9427a4b
Japheth Kiptoo <thygon6422@gmail.com>
2025-06-08 11:59:48 +0300

    Update: added twMerge and clsx to clean-up conditional styles

--- src/components/skipOption.tsx
+++ src/components/skipOption.tsx
@@ -1,4 +1,5 @@
 import type { Skip } from "../types";
+import { cn } from "../utils/cn";
 
 type Props = {
   id: string;
@@ -19,11 +20,41 @@ export const SkipOption = (props: Props) => {
   return (
     <label
       htmlFor={id}
-      className={
-        checked
-          ? "border-2 p-6 rounded-lg border-[#0037C1] bg-[#0037C1]/10"
-          : "border-2 p-6 rounded-lg bg-[#C1C1C1]/10 border-[#2A2A2A]"
-      }
+      className={cn("border-2 p-6 rounded-lg transition-all", {
+        "border-[#0037C1] bg-[#0037C1]/10": checked,
+        "bg-[#C1C1C1]/10 border-[#2A2A2A]": !checked,
+      })}
+    >
+      <input
+        id={id}
+        type="radio"
+        name={name}
+        value={value}
+        checked={checked}
+        onChange={onChange}
+        className="sr-only peer"
+      />
+
+      <div>
+        <span>{badge}</span>
+        <h2>{title}</h2>
+        <p>{hire_period}</p>
+      </div>
+    </label>
+  );
+};
+
+export const SkipOptionWithImage = (props: Props) => {
+  const { id, name, value, checked, option, onChange } = props;
+
+  const title = `${option.size} yard skip`;
+  const hire_period = `${option.hire_period_days} day hire period`;
+  const badge = `${option.size} Yards`;
+
+  return (
+    <label
+      htmlFor={id}
+      className={`border-2 p-6 rounded-lg transition-all ${checked ? "" : "bg-[#C1C1C1]/10 border-[#2A2A2A]"}`}
     >
       <input
         id={id}
