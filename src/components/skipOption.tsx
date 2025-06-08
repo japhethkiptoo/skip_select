dfa09031f9fbaff017fb4d4a0b6ed863d7b83c87
Japheth Kiptoo <thygon6422@gmail.com>
2025-06-08 15:21:08 +0300

    Update: beautify the skip_select page

--- src/components/skipOption.tsx
+++ src/components/skipOption.tsx
@@ -7,11 +7,12 @@ type Props = {
   value: string;
   checked?: boolean;
   option: Skip;
-  onChange: (event: any) => void;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onClick: (event: React.MouseEvent<HTMLInputElement>) => void;
 };
 
 export const SkipOption = (props: Props) => {
-  const { id, name, value, checked, option, onChange } = props;
+  const { id, name, value, checked, option, onChange, onClick } = props;
 
   const title = `${option.size} yard skip`;
   const hire_period = `${option.hire_period_days} day hire period`;
@@ -21,7 +22,7 @@ export const SkipOption = (props: Props) => {
   return (
     <label
       htmlFor={id}
-      className={cn("border-2 p-6 rounded-lg transition-all", {
+      className={cn("border-2 p-6 rounded-lg transition-all cursor-pointer", {
         "border-[#0037C1] bg-[#0037C1]/10": checked,
         "bg-[#C1C1C1]/10 border-[#2A2A2A] hover:border-[#0037C1]/50": !checked,
       })}
@@ -33,10 +34,29 @@ export const SkipOption = (props: Props) => {
         value={value}
         checked={checked}
         onChange={onChange}
+        onClick={onClick}
         className="sr-only peer"
       />
 
-      <div className="flex-1">
+      <div className="flex justify-between">
+        <div>
+          <span className="inline-flex items-center shadow-md rounded-full px-2 py-.5 text-sm font-medium mb-2 bg-[#0037C1] text-white">
+            {badge}
+          </span>
+          <h3 className="text-lg font-semibold capitalize mb-2 md:text-xl">
+            {title}
+          </h3>
+          <p className="text-gray-400 text-sm">{hire_period}</p>
+        </div>
+
+        <div>
+          <span className="text-xl md:text-3xl font-bold text-[#0037C1]">
+            {cost}
+          </span>
+        </div>
+      </div>
+
+      {/* <div className="flex-1">
         <span>{badge}</span>
         <h3 className="text-lg font-semibold capitalize mb-2 md:text-xl">
           {title}
@@ -46,7 +66,7 @@ export const SkipOption = (props: Props) => {
         <span className="text-xl md:text-2xl font-bold text-[#0037C1]">
           {cost}
         </span>
-      </div>
+      </div> */}
     </label>
   );
 };
--- src/components/skipOptions.tsx
+++ src/components/skipOptions.tsx
@@ -17,7 +17,7 @@ export const SkipOptions = () => {
     (option) => String(option.id) === selectedSkip,
   );
 
-  const handleSkipChange = (event: any) => {
+  const handleSkipChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const select_value = event.target.value;
     console.log(select_value, selectedSkip);
     if (selectedSkip === select_value) {
@@ -25,6 +25,14 @@ export const SkipOptions = () => {
       return;
     }
     setSelectedSkip(select_value);
+  };
+
+  const handleSkipClick = (event: React.MouseEvent<HTMLInputElement>) => {
+    const select_value = event.currentTarget.value;
+    if (selectedSkip === select_value) {
+      setSelectedSkip(null);
+      return;
+    }
   };
 
   if (loading) {
@@ -41,7 +49,7 @@ export const SkipOptions = () => {
 
   return (
     <>
-      <div className="max-w-5xl mx-auto flex flex-col gap-6 border">
+      <div className="max-w-5xl mx-auto flex flex-col gap-6 pb-32">
         <div className=" flex flex-col items-center">
           <h2 className="text-3xl font-bold mb-4 capitalize">
             Choose your skip size
@@ -61,31 +69,34 @@ export const SkipOptions = () => {
               value={option.id}
               checked={selectedSkip === String(option.id)}
               onChange={handleSkipChange}
+              onClick={handleSkipClick}
             />
           ))}
         </div>
       </div>
 
-      <div className="fixed bottom-0 left-0 right-0 bg-[#1C1C1C]  border-t border-[#2A2A2A] p-4 animate-slide-up z-50">
-        <div className="max-w-5xl mx-auto">
-          <div className="flex flex-col md:flex-row items-center justify-between">
-            <div className="flex items-center">
-              <div>
-                <span className="text-sm text-gray-400 capitalize">{`${selected_skip?.size} yard skip`}</span>
+      {selectedSkip && (
+        <div className="fixed bottom-0 left-0 right-0 bg-[#1C1C1C]  border-t border-[#2A2A2A] p-4 animate-slide-up z-50">
+          <div className="max-w-5xl mx-auto">
+            <div className="flex flex-col md:flex-row justify-between">
+              <div className="flex items-center gap-3 justify-between">
+                <div>
+                  <span className="text-sm text-gray-400 capitalize">{`${selected_skip?.size} yard skip`}</span>
+                </div>
+                <div>
+                  <span className="text-2xl font-bold text-[#0037C1]">{`£${selected_skip?.price_before_vat} `}</span>
+                  <span className="text-sm text-gray-400 ml-2">{`${selected_skip?.hire_period_days} day hire`}</span>
+                </div>
               </div>
-              <div>
-                <span className="text-2xl font-bold text-[#0037C1]">{`£${selected_skip?.price_before_vat} `}</span>
-                <span className="text-sm text-gray-400 ml-2">{`${selected_skip?.hire_period_days} day hire`}</span>
-              </div>
-            </div>
 
-            <div className="flex gap-4 items-center">
-              <button className="btn-secondary">Back</button>
-              <button className="btn-primary">Continue</button>
+              <div className="flex gap-4 items-center mt-2">
+                <button className="btn-secondary w-full">Back</button>
+                <button className="btn-primary w-full">Continue</button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
